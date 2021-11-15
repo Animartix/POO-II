6f642b28b153d9cbf8d13634b4f59aaffb3f5cc3
Animartix <animartics@gmail.com>
2021-11-15 15:37:27 -0300

    Actividad Java POO II

---
+++ src/ActividadObligatoriaPOO2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class ActividadObligatoriaPOO2 {
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
+        System.out.println("Ingrese su nombre");
+        String nombre = leer.next();
+        System.out.println("Ingrese su apellido");
+        String apellido = leer.next();
+        System.out.println("Ingrese su edad");
+        int edad = leer.nextInt();
+        System.out.println("ingrese su hobbie preferido");
+        String hobbie = leer.next();
+        System.out.println("Ingrese el sistema Operativo que utiliza");
+        String sistema = leer.next();
+    }
+}
